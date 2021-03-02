22078b12d9fe335e697ec2aef77b0b9942a20777
Epark012 <somnium.itineris@gmail.com>
2021-03-02 14:28:28 +0900

    String Test

--- Assets/JH.cs
+++ Assets/JH.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class JH : MonoBehaviour
 {
+    [SerializeField]
+    private string hellowWorld;
     // Start is called before the first frame update
     void Start()
     {
-        
+        hellowWorld = "Hellow World";
+        print(hellowWorld);
     }
 
     // Update is called once per frame
